a21ac6351990082a38a4f4705b72289335e846ee
bulbetski <bulbetski42@gmail.com>
2020-04-25 22:47:20 +0300

    4 laba

--- Laba_4/app/src/main/java/com/example/laba_4/MainActivity.java
+++ Laba_4/app/src/main/java/com/example/laba_4/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.laba_4;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
-
+    InfoRepository repository;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        repository = InfoRepository.createInstance(getApplicationContext());
+        Button button = findViewById(R.id.btn);
+        final DatePicker picker = findViewById(R.id.datePicker);
+        button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = new GregorianCalendar(picker.getYear(), picker.getMonth(), picker.getDayOfMonth());
+                Date date = calendar.getTime();
+                repository.insertDate(date);
+                stopService(new Intent(MainActivity.this, InfoService.class));
+                startForegroundService(new Intent(MainActivity.this, InfoService.class));
+                Intent intent = new Intent(MainActivity.this, MyWidget.class);
+                intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+                int[] ids = AppWidgetManager.getInstance(getApplication())
+                        .getAppWidgetIds(new ComponentName(getApplication(), MyWidget.class));
+                intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+                sendBroadcast(intent);
+            }
+        });
     }
 }
